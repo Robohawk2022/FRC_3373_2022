ad0f942c2ec6976c92a3ea2276cea6fa97d8ea3c
Ed Peters <epeters1111@gmail.com>
2022-01-30 18:10:42 -0500

    Fixing package declaration on Logger

--- src/main/java/frc/robot/util/Logger.java
+++ src/main/java/frc/robot/util/Logger.java
@@ -1,19 +1,21 @@
-public class Logger{
+package frc.robot.util;
+
+public class Logger {
 
     public static final Logger SWERVE_WHEEL = new Logger(true);
     public static final Logger SWERVE_CONTROLLER = new Logger(false);
     public static final Logger SHOOTER = new Logger(true);
     public static final Logger INTAKE = new Logger(true);
 
-    boolean show = true;
+    private final boolean show;
 
     public Logger(boolean show) {
         this.show = show;
     }
 
-    public void log(String args) {
+    public void log(String message, Object... args) {
         if (show) {
-            System.out.println(args);
+            System.out.println(String.format(message, args));
         }
     }
 }
