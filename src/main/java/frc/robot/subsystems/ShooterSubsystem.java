8f8a5fa2e4df167f3f19a24317bd878efa6c646e
Ed Peters <epeters1111@gmail.com>
2022-03-16 16:41:42 -0400

    Updates shooter to stop indexer when past position, use correct PID params, and enable up to 4 preset launch speeds

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,71 +1,106 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.motors.MotorFactory;
-import frc.robot.motors.PositionClosedLoopMotor;
-import frc.robot.motors.VelocityClosedLoopMotor;
 import frc.robot.util.Logger;
+import frc.robot.util.PIDConstant;
 
 /**
  * Subsystem for shooting
  */
 public class ShooterSubsystem {
 
-    /** Minimum speed for the launch wheel to avoid a ball getting stuck */
-    public static final double MINIMUM_LAUNCH_RPM = -2000;
+    /** PID tuning constants copied from https://github.com/REVrobotics/SPARK-MAX-Examples/blob/master/Java/Smart%20Motion%20Example/src/main/java/frc/robot/Robot.java */
+    public static final PIDConstant PID_CONSTANT = new PIDConstant(5e-5, 1e-6, 0.0, 0.0, 0.000156, -1.0, 1.0);
 
-    /** Because sometimes DigitalInput switches are reversed */
-    public static final boolean BALL_AVAILABLE_PRESSED = true;
-
-    /** Starting speed of the launch wheel */
-    public static final double STARTING_LAUNCH_RPM = -12000;
+    /** Default preset launch speeds */
+    public static final double [] LAUNCH_PRESETS = { -3900.0, -4900.0, -5900.0, -6900.0 };
 
     /** Maximum speed at which to rotate the indexer */
+    // IF INDEXER IS REVERSED - negate me
     public static final double INDEXER_MAX_SPEED = 0.5;
 
+    /** Speed tolerance for launch wheel (will only allow shot when w/in this %% of target */
+    public static final double LAUNCH_SPEED_TOLERANCE = 0.1;
+
     /** How many rotations does the indexer need to lock in a ball? */
+        // IF INDEXER IS REVERSED - negate me
     public static final double LOCKIN_ROTATIONS = 15;
 
     /** How many rotations does the indexer need to push out a ball? */
+    // IF INDEXER IS REVERSED - negate me
     public static final double SHOOT_ROTATIONS = 40;
 
     private final XboxController controller;
     private final DigitalInput ballSensor;
-    private final VelocityClosedLoopMotor launchWheel;
-    private final PositionClosedLoopMotor indexerWheel;
+
+    private final CANSparkMax launchMotor;
+    private final RelativeEncoder launchEncoder;
+    private final SparkMaxPIDController launchController;
+    private double launchTargetRpm;
+
+    private final CANSparkMax indexerMotor;
+    private final RelativeEncoder indexerEncoder;
+    private double indexerTargetPos;
+
     private boolean spinLaunchWheel;
-    private double targetLaunchSpeed;
     private int autoShotsPending;
 
     public ShooterSubsystem(XboxController controller, 
             int launchMotorPort, 
             int indexerMotorPort, 
             int ballAvailableSwitchPort) {
+
         System.err.println("initializing shooter");
+
         this.controller = controller;
-        this.ballSensor = new DigitalInput(ballAvailableSwitchPort);
-        this.launchWheel = MotorFactory.makeVelocityClosedLoopMotor("Launch", launchMotorPort);
-        this.indexerWheel = MotorFactory.makePositionClosedLoopMotor("Indexer", indexerMotorPort);
-        indexerWheel.setMaxSpeed(INDEXER_MAX_SPEED);
+        
+        ballSensor = new DigitalInput(ballAvailableSwitchPort);
+
+        launchMotor = new CANSparkMax(launchMotorPort, MotorType.kBrushless);
+        launchMotor.restoreFactoryDefaults();
+        launchMotor.setIdleMode(IdleMode.kCoast);
+        launchMotor.setClosedLoopRampRate(0.5);
+        launchEncoder = launchMotor.getEncoder();
+        launchController = launchMotor.getPIDController();
+        PID_CONSTANT.configPID(launchController);
+
+        indexerMotor = new CANSparkMax(indexerMotorPort, MotorType.kBrushless);
+        indexerMotor.restoreFactoryDefaults();
+        indexerMotor.setIdleMode(IdleMode.kBrake);
+        indexerMotor.setClosedLoopRampRate(0.1);
+        indexerEncoder = indexerMotor.getEncoder();
+
+        SmartDashboard.putNumber("Shooter Preset A", LAUNCH_PRESETS[0]);
+        SmartDashboard.putNumber("Shooter Preset B", LAUNCH_PRESETS[1]);
+        SmartDashboard.putNumber("Shooter Preset X", LAUNCH_PRESETS[2]);
+        SmartDashboard.putNumber("Shooter Preset Y", LAUNCH_PRESETS[3]);
+
         autoShotsPending = 2;
         disabledInit();
     }
 
+    // toggles the launch wheel on/off (note: this doesn't move the motor or reset target speed)
     public void setLaunchWheelEnabled(boolean enabled) {
         Logger.log("shooter: setting launch wheel to enabled=", enabled);
         spinLaunchWheel = enabled;
     }
 
+    // update the indexer position to indicate that a shot should be taken (note: doesn't move the motor)
     public void shoot() {
-        boolean atSpeed = launchWheel.getRpm() < MINIMUM_LAUNCH_RPM; // yes, less than, because speeds are negative
-        if (atSpeed) {
+        double launchCurrentRpm = indexerEncoder.getVelocity();
+        double speedDiff = Math.abs( (launchTargetRpm - launchCurrentRpm) / launchTargetRpm );
+        if (speedDiff < LAUNCH_SPEED_TOLERANCE) {
             Logger.log("shooter: shooting!");
-            indexerWheel.rotate(SHOOT_ROTATIONS);    
+            indexerTargetPos += SHOOT_ROTATIONS;
         } else {
             Logger.log("shooter: refusing to shoot; not at speed");
         }
@@ -73,20 +108,39 @@ public class ShooterSubsystem {
 
     // called 50x per second, no matter what mode we're in
     public void robotPeriodic() {
+
+        // update the world about what we're doing
         SmartDashboard.putBoolean("Launch Spinning?", spinLaunchWheel);
-        SmartDashboard.putNumber("Launch Target RPM", targetLaunchSpeed);
-        SmartDashboard.putNumber("Launch Current RPM", launchWheel.getRpm());
-        SmartDashboard.putNumber("Indexer Target Pos", indexerWheel.getTargetPosition());
-        SmartDashboard.putNumber("Indexer Current Pos", indexerWheel.getPosition());
+        SmartDashboard.putNumber("Launch Target RPM", launchTargetRpm);
+        SmartDashboard.putNumber("Launch Current RPM", launchEncoder.getVelocity());
+        SmartDashboard.putNumber("Indexer Target Pos", indexerTargetPos);
+        SmartDashboard.putNumber("Indexer Current Pos", indexerEncoder.getPosition());
         SmartDashboard.putBoolean("Ball Sensor", ballSensor.get());
+
+        // get new values for presets if necessary
+        LAUNCH_PRESETS[0] = SmartDashboard.getNumber("Shooter Preset A", LAUNCH_PRESETS[0]);
+        LAUNCH_PRESETS[1] = SmartDashboard.getNumber("Shooter Preset B", LAUNCH_PRESETS[1]);
+        LAUNCH_PRESETS[2] = SmartDashboard.getNumber("Shooter Preset X", LAUNCH_PRESETS[2]);
+        LAUNCH_PRESETS[3] = SmartDashboard.getNumber("Shooter Preset Y", LAUNCH_PRESETS[3]);
+
+        // clean up presets (make sure they are all negative in case someone didn't type in the sign)
+        for (int i=0; i<LAUNCH_PRESETS.length; i++) {
+            if (LAUNCH_PRESETS[i] > 0) {
+                LAUNCH_PRESETS[i] = -LAUNCH_PRESETS[i];
+            }
+        }
     }
 
     // called when the robot is put into disabled mode
     public void disabledInit() {
+
+        launchTargetRpm = LAUNCH_PRESETS[0];
+        indexerTargetPos = indexerEncoder.getPosition();
+        autoShotsPending = 0;
         spinLaunchWheel = false;
-        targetLaunchSpeed = STARTING_LAUNCH_RPM;
-        launchWheel.halt();
-        indexerWheel.resetClosedLoopControl();
+
+        launchMotor.set(0.0);
+        indexerMotor.set(0.0);
     }
 
     // ================================================================
@@ -95,6 +149,7 @@ public class ShooterSubsystem {
     // ================================================================
 
     public void singleShooterInit() {
+        indexerTargetPos = indexerEncoder.getPosition();
         autoShotsPending = 1;
         Logger.log("shooter: starting single shooter");
     }
@@ -147,6 +202,12 @@ public class ShooterSubsystem {
         updateIndexerWheel();
     }
 
+    public void teleopInit() {
+        indexerTargetPos = indexerEncoder.getPosition();
+        launchTargetRpm = LAUNCH_PRESETS[0];
+        spinLaunchWheel = false;
+    }
+
     // called 50x per second in teleop mode
     public void teleopPeriodic() {
         updateLaunchWheel();
@@ -162,32 +223,32 @@ public class ShooterSubsystem {
         // start button toggles the launch wheel on and off
         if (controller.getStartButtonPressed()) {
             setLaunchWheelEnabled(!spinLaunchWheel);
-            PreSetSpeeds();
         }
 
         // if the launch wheel is spinning, we'll allow speed changes
         if (spinLaunchWheel) {
 
-            // A button: reset launch speed
             if (controller.getAButtonPressed()) {
-                targetLaunchSpeed = STARTING_LAUNCH_RPM;
-                Logger.log("shooter: reset launch wheel to ", targetLaunchSpeed);
+                launchTargetRpm = LAUNCH_PRESETS[0];
+                Logger.log("shooter: reset launch wheel to ", launchTargetRpm);
             }
-            // X button: go 10% slower
+            else if (controller.getBButtonPressed()) {
+                launchTargetRpm = LAUNCH_PRESETS[1];
+                Logger.log("shooter: reset launch wheel to ", launchTargetRpm);
+            }
             else if (controller.getXButtonPressed()) {
-                targetLaunchSpeed *= 0.975;
-                Logger.log("shooter: slowed down launch wheel to ", targetLaunchSpeed);
+                launchTargetRpm = LAUNCH_PRESETS[2];
+                Logger.log("shooter: reset launch wheel to ", launchTargetRpm);
             }
-            // Y button: go 10% faster
             else if (controller.getYButtonPressed()) {
-                targetLaunchSpeed *= 1.025;
-                Logger.log("shooter: sped up launch wheel to ", targetLaunchSpeed);
+                launchTargetRpm = LAUNCH_PRESETS[3];
+                Logger.log("shooter: reset launch wheel to ", launchTargetRpm);
             }
 
-            launchWheel.setRpm(targetLaunchSpeed);
+            launchController.setReference(launchTargetRpm, ControlType.kVelocity);
         }
         else {
-            launchWheel.coast();
+            launchMotor.set(0.0);
         }
     }
 
@@ -196,46 +257,23 @@ public class ShooterSubsystem {
      */
     public void updateIndexerWheel() {
 
-        // if someone hits the left bumper, we'll reset position loop control
-        if (controller.getLeftBumperPressed()) {
-            indexerWheel.resetClosedLoopControl();
-        }
-
-        // trigger indexing IF there is a ball in front of the sensor now,
-        // and there wasn't one last time
+        // trigger indexing if there is a ball waiting
         if (ballSensor.get()) {
             Logger.log("shooter: rotating for intake");
-            indexerWheel.rotate(LOCKIN_ROTATIONS);
+            indexerTargetPos += LOCKIN_ROTATIONS;
         }
 
-        // if someone wants to shoot, and the wheel's at speed and we 
-        // have a ball, go for it!
+        // trigger shooting if someone wants to shoot
         if (controller.getBButtonPressed()) {
-            System.err.println("**************************************");
-            System.err.println("**************************************");
-            System.err.println("**************************************");
-            System.err.println("**************************************");
-            System.err.println("**************************************");
-            System.err.println("**************************************");
-            System.err.println("**************************************");
             shoot();
         }
 
-        indexerWheel.updateSpeed();
-    }
-    /**Allows drive to select a preset shooting speed for shooting */
-    protected void PreSetSpeeds() {
-        if(controller.getXButton() == true) {
-            targetLaunchSpeed = STARTING_LAUNCH_RPM;
-        }
-        else if(controller.getYButton() == true) {
-            targetLaunchSpeed = STARTING_LAUNCH_RPM;
-        }
-        else if(controller.getAButton() == true) {
-            targetLaunchSpeed = STARTING_LAUNCH_RPM;
-        }
-        else if(controller.getBButton() == true) {
-            targetLaunchSpeed = STARTING_LAUNCH_RPM;
+        // if the indexer is behind where it needs to be, run it
+        // IF INDEXER IS REVERSED - turn this into ">"
+        if (indexerTargetPos > indexerEncoder.getPosition()) {
+            indexerMotor.set(INDEXER_MAX_SPEED);
+        } else {
+            indexerMotor.set(0.0);
         }
     }
 }
