1fdbd3cfb3a0491590c93991f61dd5b6a915f9b5
Mac L <macattackairman@gmail.com>
2022-03-19 15:20:24 -0400

    added old speed

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -387,7 +387,7 @@ public class Robot extends TimedRobot {
   public void macDrive(double leftX, double leftY, double rightX) {
 
     // double moveSpeed = Math.sqrt(leftX * leftX + leftY * leftY) * MAX_DRIVE_POWER * turboFactor * reverseFactor;
-    double moveSpeed = (leftY / 3);
+    double moveSpeed = Math.sqrt(leftX * leftX + leftY * leftY) * MAX_DRIVE_POWER * turboFactor * reverseFactor;;
     // double oldTurnAngle = leftX * leftX * leftX * MAX_ROTATION_POWER * reverseFactor;    
     // double safeTurnAngle = 4;
     double turnAngle = leftX * leftX * leftX * MAX_ROTATION_POWER * reverseFactor;
